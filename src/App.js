578e18d263298c57c73d914f935702e97668fe77
mohamadyaserakbari <myaser.akbari@gmail.com>
2022-12-21 21:00:39 +0330

    I add Monster id and name with an API which have info of a 10 user.

--- src/App.js
+++ src/App.js
@@ -5,35 +5,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monters: [
-        {
-          id: '1212a',
-          name: 'Linda'
-        },
-        {
-          id: '1213b',
-          name: 'Frank'
-        },
-        {
-          id: '1214c',
-          name: 'Jack'
-        },
-        {
-          id: '1215d',
-          name: 'Daxty'
-        }
-      ]
+      monsters: []
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then(
+        (users) => this.setState(
+          () => { return { monsters: users }; },
+          () => { console.log(this.state); }
+        )
+      );
   }
 
   render() {
     return (
       <div className='App'>
         {
-          this.state.monters.map((monster) => {
+          this.state.monsters.map((monster) => {
             return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
+              <div key={monster.id} style={{ display: 'flex', alignItems: 'center', marginTop: '16px' }}>
+                <h3 style={{ margin: '0px 4px 0px 24px', fontWeight: '300' }}>{monster.id + ' - '}</h3>
+                <h2 style={{ margin: '0px 4px 0px 0px', fontWeight: '500' }}>Name: </h2>
+                <h3 style={{ margin: '24px 8px', fontWeight: '350' }}>{monster.name}</h3>
               </div>
             );
           })
